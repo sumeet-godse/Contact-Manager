b37a374533a443e2e9694a3f0fb120544e06b587
Sumeet Godse <sgodse@deqode.com>
2021-07-30 15:59:47 +0530

    Fixed Add Data Bug which wanted us to refresh everytime we added new data

--- contact-manager/src/components/DisplayData.jsx
+++ contact-manager/src/components/DisplayData.jsx
@@ -2,14 +2,24 @@ import getRecords from "../utilities/utility";
 import "../styles/DisplayData.css"
 import DisplaySelectedRecord from "./DisplaySelectedRecord";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const DisplayData = () => {
-  const records = getRecords();
+  let records = getRecords();
   const [selectedRecord, setSelectedRecord] = useState(0);
+  const [newFlag, setNewFlag] = useState(false);
 
   const handleRecordChange = (index) => {
     setSelectedRecord(index);
   }
+
+  const handleNewFlag = () => {
+    setNewFlag(!newFlag);
+  }
+
+  useEffect(() => {
+    records = getRecords();
+  }, [newFlag]);
 
   return(
     <div className = "display-data-container">
@@ -24,7 +34,7 @@ const DisplayData = () => {
           })
         }
       </div>
-      <DisplaySelectedRecord selectedRecord = {selectedRecord} />
+      <DisplaySelectedRecord handleNewFlag = {handleNewFlag} selectedRecord = {selectedRecord} />
     </div>
   );
 }
