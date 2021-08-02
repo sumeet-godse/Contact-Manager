e2368058f73502a00dc1aaa5969d83363817c12f
Sumeet Godse <sgodse@deqode.com>
2021-08-02 15:01:18 +0530

    added validation for add/edit data

--- contact-manager/src/components/AddData.jsx
+++ contact-manager/src/components/AddData.jsx
@@ -9,15 +9,36 @@ const AddData = (props) => {
   const [address, setAddress] = useState("");
   const [records, setRecords] = useState(decryptLoad("encryptedRecords"));
 
+  const phoneRegexp = /^[0-9]{10}$/;
+  const emailRegexp = /\S+@\S+\.\S+/;
+
   const handleAdd = () => {
-    records.push({
-      phone,
-      email,
-      address
-    })
-    setRecords(records);
-    encryptStore(records);
-    props.handleAddFlag();
+    if( !phoneRegexp.test(phone) || !emailRegexp.test(email) || !address ) {
+      if( !phoneRegexp.test(phone) ) {
+        document.getElementById("add-phone-error").innerHTML = "Invalid Phone";
+      } else {
+        document.getElementById("add-phone-error").innerHTML = "";
+      }
+      if( !emailRegexp.test(email) ) {
+        document.getElementById("add-email-error").innerHTML = "Invalid Email";
+      } else {
+        document.getElementById("add-email-error").innerHTML = "";
+      }
+      if( !address ) {
+        document.getElementById("add-address-error").innerHTML = "Address cannot be empty";
+      } else {
+        document.getElementById("add-address-error").innerHTML = "";
+      }
+    } else {
+      records.push({
+        phone,
+        email,
+        address
+      })
+      setRecords(records);
+      encryptStore(records);
+      props.handleAddFlag();
+    }
   }
 
   const handlePhone = (e) => {
@@ -40,12 +61,15 @@ const AddData = (props) => {
         </span>
         <div id = "add-data-phone" className = "common">
           <input id = "add-phone-input" onChange = {(e) => handlePhone(e)} type = "text" placeholder = "Phone" value = {phone} />
+          <p id = "add-phone-error" />
         </div>
         <div id = "add-data-email" className = "common">
           <input id = "add-email-input" onChange = {(e) => handleEmail(e)} type = "text" placeholder = "Email" value = {email} />
+          <p id = "add-email-error" />
         </div>
         <div id = "add-data-address" className = "common">
           <textarea id = "add-address-input" rows = "4" onChange = {(e) => handleAddress(e)} placeholder = "Address" value = {address} />
+          <p id = "add-address-error" />
         </div>
         <div id = "add-data-button" className = "common">
           <input id = "add-button-input" onClick = {handleAdd} type = "button" value = "Submit" />
--- contact-manager/src/components/EditData.jsx
+++ contact-manager/src/components/EditData.jsx
@@ -9,13 +9,34 @@ const EditData = ({ handleEditFlag, index, handleEditClose }) => {
   const [email, setEmail] = useState(records[index].email);
   const [address, setAddress] = useState(records[index].address);
 
+  const phoneRegexp = /^[0-9]{10}$/;
+  const emailRegexp = /\S+@\S+\.\S+/;
+
   const handleEdit = () => {
+    if( !phoneRegexp.test(phone) || !emailRegexp.test(email) || !address ) {
+      if( !phoneRegexp.test(phone) ) {
+        document.getElementById("edit-phone-error").innerHTML = "Invalid Phone";
+      } else {
+        document.getElementById("edit-phone-error").innerHTML = "";
+      }
+      if( !emailRegexp.test(email) ) {
+        document.getElementById("edit-email-error").innerHTML = "Invalid Email";
+      } else {
+        document.getElementById("edit-email-error").innerHTML = "";
+      }
+      if( !address ) {
+        document.getElementById("edit-address-error").innerHTML = "Address cannot be empty";
+      } else {
+        document.getElementById("edit-address-error").innerHTML = "";
+      }
+    } else {
       records[index].phone = phone;
       records[index].email = email;
       records[index].address = address;
       setRecords(records);
       encryptStore(records);
       handleEditFlag();
+    }
   }
 
   const handlePhone = (e) => {
@@ -38,12 +59,15 @@ const EditData = ({ handleEditFlag, index, handleEditClose }) => {
         </span>
         <div id = "edit-data-phone" className = "common">
           <input id = "edit-phone-input" onChange = {(e) => handlePhone(e)} type = "text" placeholder = "Phone" value = {phone} />
+          <p id = "edit-phone-error" />
         </div>
         <div id = "edit-data-email" className = "common">
           <input id = "edit-email-input" onChange = {(e) => handleEmail(e)} type = "text" placeholder = "Email" value = {email} />
+          <p id = "edit-email-error" />
         </div>
         <div id = "edit-data-address" className = "common">
           <textarea id = "edit-address-input" rows = "4" onChange = {(e) => handleAddress(e)} type = "text" placeholder = "Address" value = {address} />
+          <p id = "edit-address-error" />
         </div>
         <div id = "edit-data-submit" className = "common">
           <input id = "edit-submit-input" onClick = {handleEdit} type = "button" value = "Submit" />
